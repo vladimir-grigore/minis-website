c219b32a7efe7cb7fa0c70ccfa242925bfb77d84
Vladimir Grigore <vladimirgrigore@Vladimirs-MacBook-Pro.local>
2017-11-22 19:05:23 -0800

    Check Heroku pipeline

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
-            <h4 className="App-title">Now on Heroku</h4>
           </header>
           <p className="App-intro">
           </p>
