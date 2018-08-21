fe39a0e41e58f48413f9f20db9abd3d8c437fca2
Vladimir Grigore <vladimirgrigore@Vladimirs-MacBook-Pro.local>
2018-08-20 22:03:53 -0700

    Add Infinity images

--- src/pages/PhotoSet.js
+++ src/pages/PhotoSet.js
@@ -1,43 +1,103 @@
 const photoSet = [
   {
-    src: "./heroImage.jpg",
-    width: 1,
+    src: "./images/20171103_083725.jpg",
+    width: 2,
     height: 1,
   },
   {
-    src: "./heroImage.jpg",
-    width: 1,
+    src: "./images/20171103_083736.jpg",
+    width: 2,
+    height: 1.1,
+  },
+  {
+    src: "./images/20171103_083849.jpg",
+    width: 2,
     height: 1,
   },
   {
-    src: "./heroImage.jpg",
-    width: 1,
+    src: "./images/20171103_084007.jpg",
+    width: 2,
+    height: 1.1,
+  },
+  {
+    src: "./images/20180217_123756.jpg",
+    width: 2,
+    height: 3,
+  },
+  {
+    src: "./images/20171103_084014.jpg",
+    width: 2,
+    height: 1.1,
+  },
+  {
+    src: "./images/20171218_084504.jpg",
+    width: 2,
+    height: 1.1,
+  },
+  {
+    src: "./images/20180217_122713.jpg",
+    width: 2,
+    height: 1.1,
+  },
+  {
+    src: "./images/20180217_122750.jpg",
+    width: 2,
+    height: 1.1,
+  },
+  {
+    src: "./images/20180217_122843.jpg",
+    width: 2,
+    height: 1.1,
+  },
+  {
+    src: "./images/20180217_123214.jpg",
+    width: 2,
+    height: 1.2,
+  },
+  {
+    src: "./images/20180217_123248.jpg",
+    width: 2,
+    height: 1.2,
+  },
+  {
+    src: "./images/20180217_123327.jpg",
+    width: 2,
+    height: 1.2,
+  },
+  {
+    src: "./images/20180311_170438.jpg",
+    width: 2,
+    height: 1.2,
+  },
+  {
+    src: "./images/20180327_185915.jpg",
+    width: 2, //brown
+    height: 1.2,
+  },
+  {
+    src: "./images/20180327_190137.jpg",
+    width: 1.2,
     height: 1,
   },
   {
-    src: "./heroImage.jpg",
-    width: 1,
+    src: "./images/20180327_190153.jpg",
+    width: 1.5,
     height: 1,
   },
   {
-    src: "./heroImage.jpg",
-    width: 1,
-    height: 1,
+    src: "./images/20171218_083459.jpg",
+    width: 2, //white
+    height: 1.1,
   },
   {
-    src: "./heroImage.jpg",
-    width: 1,
-    height: 1,
+    src: "./images/20171218_083635.jpg",
+    width: 2, //white
+    height: 1.1,
   },
   {
-    src: "./heroImage.jpg",
-    width: 1,
-    height: 1,
-  },
-  {
-    src: "./heroImage.jpg",
-    width: 1,
-    height: 1,
+    src: "./images/20171218_083906.jpg",
+    width: 2, //white
+    height: 1.1,
   },
 ]
 
